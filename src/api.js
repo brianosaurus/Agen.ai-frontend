dbcb5fb5867305917bccc66841f0fe569aa08890
Brian Woods <me@brian.biz>
2023-05-04 18:13:05 -0600

    adds a route

--- src/api.js
+++ src/api.js
@@ -1,27 +1,30 @@
 import { request } from 'jsonrpc-lite';
 
-const apiUrl = 'http://localhost:5552'; // Replace with your backend server URL
+const apiUrl = 'http://localhost:5552/api'; // Replace with your backend server URL
 
 export async function getImages(constraintKeywords, imageConstraints, subject) {
-  const requestData = request(
-    1, // Request ID (can be any number)
-    'generate_image', // Method name
-    { constraintKeywords, imageConstraints, subject } // Parameters object
-  );
+  const payload = {
+    jsonrpc: '2.0',
+    method: 'generate_image',
+    params: { constraintKeywords: constraintKeywords, imageConstraints: imageConstraints, subject: subject },
+    id: 1,
+  };
 
   const response = await fetch(apiUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(requestData)
+    body: JSON.stringify(payload)
   });
 
   const responseBody = await response.json();
+
+  console.log(responseBody)
 
   if (responseBody.error) {
     throw new Error(responseBody.error.message);
   }
 
-  return responseBody.result;
+  return responseBody;
 }
